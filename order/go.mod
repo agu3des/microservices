e38cf8ced6e3d45deb8b91ca6c8357d84809122b
Ananda Guedes <anandaguedesdoo@gmail.com>
2026-01-20 10:46:08 -0300

    parte 04

--- order/go.mod
+++ order/go.mod
@@ -11,6 +11,8 @@ require (
 	gorm.io/gorm v1.31.1
 )
 
+require github.com/grpc-ecosystem/go-grpc-middleware v1.4.0
+
 require (
 	filippo.io/edwards25519 v1.1.0 // indirect
 	github.com/go-sql-driver/mysql v1.8.1 // indirect
--- order/internal/adapters/payment/payment.go
+++ order/internal/adapters/payment/payment.go
@@ -2,40 +2,66 @@ package payment
 
 import (
 	"context"
+	"fmt"
+	"log"
+	"time"
 
-	"github.com/agu3des/microservices-proto/golang/payment"
 	"github.com/agu3des/microservices/order/internal/application/core/domain"
+	
+	"github.com/agu3des/microservices-proto/golang/payment"
+	
+	grpc_retry "github.com/grpc-ecosystem/go-grpc-middleware/retry"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/status"
 )
 
 type Adapter struct {
-	payment payment.PaymentClient // come from the generated code by the protobuf compiler
+	paymentServiceUrl string
 }
 
 func NewAdapter(paymentServiceUrl string) (*Adapter, error) {
-	var opts []grpc.DialOption
-	
-	opts = append(opts, grpc.WithTransportCredentials(insecure.NewCredentials()))
-	
-	conn, err := grpc.Dial(paymentServiceUrl, opts...)
-	if err != nil {
-		return nil, err
-	}
-	
-	// Nota: Em uma implementação real, você precisará gerenciar o fechamento 
-	// desta conexão (conn.Close()) quando a aplicação encerrar.
-	
-	client := payment.NewPaymentClient(conn) // initialize the stub
-	return &Adapter{payment: client}, nil
+	return &Adapter{
+		paymentServiceUrl: paymentServiceUrl,
+	}, nil
 }
 
 func (a *Adapter) Charge(order *domain.Order) error {
-	_, err := a.payment.Create(context.Background(), &payment.CreatePaymentRequest{
+	opts := []grpc.DialOption{
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+		grpc.WithUnaryInterceptor(grpc_retry.UnaryClientInterceptor(
+			grpc_retry.WithCodes(codes.Unavailable, codes.ResourceExhausted),
+			grpc_retry.WithMax(5),
+			grpc_retry.WithBackoff(grpc_retry.BackoffLinear(1 * time.Second)),
+		)),
+	}
+
+	conn, err := grpc.NewClient(a.paymentServiceUrl, opts...)
+	if err != nil {
+		return fmt.Errorf("falha ao conectar no serviço de pagamento: %v", err)
+	}
+	defer conn.Close()
+
+	client := payment.NewPaymentClient(conn)
+
+	req := &payment.CreatePaymentRequest{
 		UserId:     order.CustomerID,
 		OrderId:    order.ID,
 		TotalPrice: order.TotalPrice(),
-	})
-	
-	return err
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
+	defer cancel()
+
+	_, err = client.Create(ctx, req)
+	if err != nil {
+		if status.Code(err) == codes.DeadlineExceeded {
+			log.Println("LOG: Timeout exceeded calling Payment Service (DeadlineExceeded)")
+		}
+		return fmt.Errorf("erro no pagamento: %v", err)
+	}
+
+	fmt.Printf("Pagamento autorizado externamente para o pedido %d\n", order.ID)
+	return nil
 }
